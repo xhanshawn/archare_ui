d7ebeb1264d678b30e15b2e2c6f35508f91d7c6e
xhanshawn <xhan@wpi.edu>
2016-06-22 22:01:44 -0400

    add archare ui framework js part

---
+++ app/assets/javascripts/ah-ui-js.js
@@ -0,0 +1,50 @@
+
+// menu toggle for sb
+$('.ah-menu-toggle').on('click', function(e){
+	$('.ah-wrapper').toggleClass('sb-collapsed');
+});
+
+
+// tooltip for ah nav cube
+$(document).ready(function(){
+
+  console.log("Archare-ui-js");
+
+  $('.ah-nav-cube').attr({
+    'data-toggle': 'tooltip',
+    'data-delay' : '{"show":"1200", "hide":"0"}'
+  });
+
+
+  $('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
+});
+
+
+// scroll animation for navigating with id
+$(document).on('click', 'a[href^="#"]', function(e) {
+  // target element id
+  var id = $(this).attr('href');
+
+  // target element
+  var $id = $(id);
+
+  // prevent standard hash navigation (avoid blinking in IE)
+  e.preventDefault();
+
+  // var animate_options = {duration: 800, easing: 'easeOutSine'};
+
+  var pos = 0;
+
+  if ($id.length !== 0) {
+
+    // top position relative to the document
+    pos = $(id).offset().top - 60;
+  }
+
+  var diff = Math.abs(pos - $(document).scrollTop());
+
+  console.log(diff);
+  // animated top scrolling
+  $('body, html').animate({scrollTop: pos}, { duration: diff, easing: 'easeInOutCubic'});
+
+});
--- app/assets/javascripts/test.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(d){
-	console.log("archare-ui");
-});
